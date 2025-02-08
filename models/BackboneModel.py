69c8b14cd7abd7ff2c6d895ce72c669b9c949393
Bowen (Bryan) Wang <bryanwang.nlp@connect.hku.hk>
2025-02-08 23:50:50 +0800

    update: add gemini new model support and qwen2.5 vl

--- models/BackboneModel.py
+++ models/BackboneModel.py
@@ -16,7 +16,11 @@ SUPPORTED_MODELS = {
         "gemini/gemini-1.5-flash",
         'gemini/gemini-1.5-pro', # 'gemini/gemini-1.5-pro-latest', 'gemini-1.5-pro-exp-0827', 'gemini-1.5-pro-002'
         'gemini/gemini-exp-1121',
+        'gemini/gemini-2.0-flash',
+        'gemini/gemini-2.0-flash-thinking-exp-01-21',
+        'gemini/gemini-2.0-pro-exp-02-05',
         
+        'openai/qwen2.5-vl-72b-instruct',
         # "claude-3-5-sonnet-20241022",
         # 'claude-3-haiku-20240307'
         # 'claude-3-opus-20240229',
